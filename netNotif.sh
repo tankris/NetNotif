95cf07f17bf03ad943769ede9f02c816a98406a0
Tanishq K <kronorak@outlook.com>
2021-07-13 07:01:43 +0530

    Update netNotif.sh
    - Script now displays a notification using dunst daemon when the speed
    of the net is below 500 Mbps

--- netNotif.sh
+++ netNotif.sh
@@ -4,5 +4,5 @@ downSpeed=$($HOME/go/bin/ndt7-client|awk '/Download/'|awk '{printf "%.0f",$2}')
 # If the speed is below 200 Mbps, print
 if [[ ! -z "$downSpeed" && $downSpeed -lt 500 ]] 
 then
-	echo "Hey"
+	$(dunstify -h string:x-dunst-stack-tag:netnotif -u c "Internet Speed Notification!" "<b>Low Speed</b>")
 fi
